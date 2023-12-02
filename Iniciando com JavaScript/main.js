1065ae7f3b6b189d4c7b23c298bc5a752bfcd613
Gabriel Borba <134644804+Leaoooo@users.noreply.github.com>
2023-12-02 19:50:08 -0300

    Add files via upload

---
+++ Iniciando com JavaScript/main.js
@@ -0,0 +1,59 @@
+// Você pode apresentar o resultado tanto no **console** quanto em um **alerta** no navegador.
+// Para saber o tipo de dado você pode usar o operador `typeof`
+
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+// alert("Hello World!");
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+// let number1 = Number(prompt("Digite o primeiro número: "));
+// let number2 = Number(prompt("Digite o segundo número: "));
+// const sum = number1 + number2;
+// alert(sum);
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+// let valueToVerify = prompt("Digite o valor para verificarmos: ");
+// let checkValue = !isNaN(valueToVerify) ? "É um número" : "Não é um número";
+// alert(checkValue);
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+// let valueToVerify = prompt("Digite o valor para verificarmos: ");
+// let checkValue = isNaN(valueToVerify) ? "É uma string" : "Não é uma string";
+// alert(checkValue);
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+// let valueToVerify = prompt("Digite o valor para verificarmos: ");
+// let checkValue =
+//   valueToVerify == "true" || valueToVerify == "false"
+//     ? "É um booleano"
+//     : "Não é um booleano";
+// alert(checkValue);
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+// let number1 = Number(prompt("Digite o primeiro número: "));
+// let number2 = Number(prompt("Digite o segundo número: "));
+// const sub = number1 - number2;
+// alert(sub);
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// let number1 = Number(prompt("Digite o primeiro número: "));
+// let number2 = Number(prompt("Digite o segundo número: "));
+// const mult = number1 * number2;
+// alert(mult);
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// let number1 = Number(prompt("Digite o primeiro número: "));
+// let number2 = Number(prompt("Digite o segundo número: "));
+// const div = number1 / number2;
+// alert(div);
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// let valueToVerify = prompt("Digite o valor para verificarmos: ");
+// let checkValue =
+//   valueToVerify % 2 == 0 ? "É um número par" : "Não é um número par";
+// alert(checkValue);
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+// let valueToVerify = prompt("Digite o valor para verificarmos: ");
+// let checkValue =
+//   valueToVerify % 2 !== 0 ? "É um número ímpar" : "Não é um número ímpar";
+// alert(checkValue);
